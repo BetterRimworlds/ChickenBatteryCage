b2d3c706844e568604cb381fb6d6120c6ca43fad
Theodore R. Smith <hopeseekr@gmail.com>
2026-09-20 16:03:03 +0300

    Added the four-tier battery chicken cage

---
+++ Source/Building_ChickenBatteryCage.cs
@@ -0,0 +1,19 @@
+/*
+ * This file is part of BetterRimworlds.ChickenBatteryCage, a Better Rimworlds Project.
+ *
+ * Copyright © 2026 Theodore R. Smith
+ * Author: Theodore R. Smith <hopeseekr@gmail.com>
+ *   GPG Fingerprint: D8EA 6E4D 5952 159D 7759  2BB4 EEB6 CE72 F441 EC41
+ *   https://github.com/BetterRimworlds/BetterRimworlds.ChickenBatteryCage
+ *
+ * This file is licensed under the MIT License.
+ */
+
+using Verse;
+
+namespace BetterRimworlds.ChickenBatteryCage;
+
+public class Building_ChickenBatteryCage : Building
+{
+    public const int ChickenCapacity = 10;
+}
